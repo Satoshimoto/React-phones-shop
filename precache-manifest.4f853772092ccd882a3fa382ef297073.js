5de9fd5c912e97a3e0ef567cd7132dab8bb456d5
Shimakaze <szymorzel@gamil.com>
2019-04-06 15:21:19 +0200

    Updates

---
+++ precache-manifest.4f853772092ccd882a3fa382ef297073.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "9472bc1926ad5d54fcd93ff420a5855f",
+    "url": "/React-phones-shop/static/media/iphoneXS.9472bc19.png"
+  },
+  {
+    "revision": "2f845d9a6e839ceefea1",
+    "url": "/React-phones-shop/static/css/main.788d1efe.chunk.css"
+  },
+  {
+    "revision": "e7c2f31ca4b8923aa4b3",
+    "url": "/React-phones-shop/static/js/runtime~main.15fb0eaf.js"
+  },
+  {
+    "revision": "3d3d98c8c7b62c4998f0",
+    "url": "/React-phones-shop/static/js/2.1f77e07a.chunk.js"
+  },
+  {
+    "revision": "1c3249e48e0db11ca2333af303f99af7",
+    "url": "/React-phones-shop/static/media/samsung.1c3249e4.png"
+  },
+  {
+    "revision": "0e2c05f22575b27db956caceb121eb65",
+    "url": "/React-phones-shop/static/media/lg.0e2c05f2.png"
+  },
+  {
+    "revision": "8e649a217a02fefe9ac74e2c4bb42740",
+    "url": "/React-phones-shop/static/media/iphonex.8e649a21.png"
+  },
+  {
+    "revision": "1994ecaf0718e29879258d22a845d64e",
+    "url": "/React-phones-shop/static/media/mate20.1994ecaf.png"
+  },
+  {
+    "revision": "1b252140d3ccda1a39f3c648110c5579",
+    "url": "/React-phones-shop/static/media/pixel3.1b252140.png"
+  },
+  {
+    "revision": "2f845d9a6e839ceefea1",
+    "url": "/React-phones-shop/static/js/main.bc9e063c.chunk.js"
+  },
+  {
+    "revision": "a911833d51c286c7535ed611cf9cf64e",
+    "url": "/React-phones-shop/static/media/LG_g7.a911833d.png"
+  },
+  {
+    "revision": "310d76c021901f6313fd5735107cd205",
+    "url": "/React-phones-shop/static/media/moto_g7.310d76c0.png"
+  },
+  {
+    "revision": "a686c356ada55f0674a40de4b719e6fa",
+    "url": "/React-phones-shop/static/media/iphone7.a686c356.png"
+  },
+  {
+    "revision": "5bdbbdd1cef91726b48159dceb1d567c",
+    "url": "/React-phones-shop/static/media/razer_phone_2.5bdbbdd1.png"
+  },
+  {
+    "revision": "60b222db6101ace7bef1f1074893bdd8",
+    "url": "/React-phones-shop/static/media/htc_u12.60b222db.png"
+  },
+  {
+    "revision": "7b9649d0a26989e6d8034909ebcb310c",
+    "url": "/React-phones-shop/static/media/lg_v40.7b9649d0.png"
+  },
+  {
+    "revision": "5ed754a67b612368b8c33bf4aa802954",
+    "url": "/React-phones-shop/static/media/galaxy_s10.5ed754a6.png"
+  },
+  {
+    "revision": "c07370f61eed53a73cf5b41309b759e0",
+    "url": "/React-phones-shop/index.html"
+  }
+];
